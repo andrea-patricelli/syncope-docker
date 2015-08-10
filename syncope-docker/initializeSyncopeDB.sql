ec5cd6d9f65d49220a5e45a7ecac32cf6929820f
Andrea Patricelli <andrea.patricelli@tirasa.net>
2015-08-10 09:11:21 +0200

    clean branch

--- syncope-docker/initializeSyncopeDB.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS syncope;
-
-GRANT ALL PRIVILEGES ON syncope.* TO 'syncope'@'localhost' IDENTIFIED BY 'syncope';
--- syncope-docker/setenv.sh
+++
@@ -1,1 +0,0 @@
-JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -Xms1536m -Xmx1536m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m -XX:+DisableExplicitGC"
